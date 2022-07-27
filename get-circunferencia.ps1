5497fe1c6676b5e28cc1633f6733b62b2dcfb63d
Wilson Sekiguti <wiseki@microsoft.com>
2022-07-27 10:12:39 -0300

    inclusão de informações de ajuda no script get-circunferencia.ps1

--- get-circunferencia.ps1
+++ get-circunferencia.ps1
@@ -1,3 +1,16 @@
+<#
+.SYNOPSIS
+    script que retorna a circunferencia com o raio informado
+.DESCRIPTION
+    script que retorna a circunferencia  com o raio informado como parametro
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    https://github.com/wilsonmse/workshopdemo
+.EXAMPLE
+    get-circunferencia.ps1 -raio n
+    onde n: é um numero inteiro
+#>
 [CmdletBinding()]
 param (
     [int]$raio
