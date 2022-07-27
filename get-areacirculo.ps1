60b2232fa646dce40d9c59fa0ef514fc89eb98ae
Wilson Sekiguti <wiseki@microsoft.com>
2022-07-27 09:54:05 -0300

    adicionado informação de ajuda no script

--- get-areacirculo.ps1
+++ get-areacirculo.ps1
@@ -1,3 +1,17 @@
+<#
+.SYNOPSIS
+    script que retorna a area de um circulo com o raio informado
+.DESCRIPTION
+    script que retorna a area de um circulo com o raio informado como parametro
+.NOTES
+    Information or caveats about the function e.g. 'This function is not supported in Linux'
+.LINK
+    https://github.com/wilsonmse/workshopdemo
+.EXAMPLE
+    get-areacirculo.ps1 -raio n
+    onde n: é um numero inteiro
+#>
+
 [CmdletBinding()]
 param (
     [int]$raio
